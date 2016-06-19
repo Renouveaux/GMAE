d7b6b6ade0f8f9cc86132f65f55e8ce1922b5631
Renouveaux <renouveaux@hotmail.com>
2016-06-19 17:07:11 +0200

    Ajout des fichiers pour le suivi GIT

---
+++ app/components/billing/index.js
@@ -0,0 +1,19 @@
+module.exports = function($scope){
+
+	$scope.status = {
+		isopen: false
+	};
+
+	$scope.toggled = function(open) {
+		console.log('Dropdown is now: ', open);
+	};
+
+	$scope.toggleDropdown = function($event) {
+		$event.preventDefault();
+		$event.stopPropagation();
+		$scope.status.isopen = !$scope.status.isopen;
+	};
+
+	$scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
+
+}
